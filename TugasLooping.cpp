7be8e5a34725c8d75b1bd1efa66f50dace75a83c
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-30 22:43:26 +0700

    Membuat fungsi isFibonacci

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -19,3 +19,18 @@ bool isPrima(int n) {
     return true;
 }
 
+// Function untuk cek bilangan Fibonacci
+bool isFibonacci(int n) {
+    int a = 0, b = 1, c = 0;
+
+    while (c <= n) {
+        if (c == n) {
+            return true;
+        }
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return false;
+}
+
