d31551be9fd3f0e8ea7f8e3ab1f4ea94599a255d
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-30 22:49:19 +0700

    Membuat fungsi menu

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -58,3 +58,12 @@ void tampilHasilFibonacci() {
     }
 }
 
+// Function menu
+void menu() {
+    cout << "\n=== MENU ===\n";
+    cout << "1. Cek Bilangan Prima\n";
+    cout << "2. Cek Bilangan Fibonacci\n";
+    cout << "0. Keluar\n";
+    cout << "Pilih menu: ";
+}
+
